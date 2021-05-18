25060db76eeaf50c5f308763094ffe465bb0e62d
Robert Erickson <bobericksonjr@gmail.com>
2021-05-18 15:04:34 -0400

    adds nav bar

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ const App = (() => {
 
 
 
-    const item1 = Item("clean", "clean the whole house", "tomorrow", "level 1");
+    const item1 = Task("clean", "clean the whole house", "tomorrow", "level 1");
     window.item1 = item1;
 
-    const item2 = Item("work", "work hard", "yesterday", "level 2");
+    const item2 = Task("work", "work hard", "yesterday", "level 2");
     window.item2 = item2;
 
-    const item3 = Item("study", "study all day", "end of year", "level 3");
+    const item3 = Task("study", "study all day", "end of year", "level 3");
     window.item3 = item3;
 
     const proj1 = Project("house chores", "daily house chores", "Friday", "level 5");
